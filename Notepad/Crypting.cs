106fd442490d9cf03fa5a2cef46be008d2967d5d
Kuda-ob84 <65267697+Kuda-ob84@users.noreply.github.com>
2020-10-06 11:16:58 +0600

    Добавьте файлы проекта.

---
+++ Notepad/Crypting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad
+{
+    public class Crypting
+    {
+        static string password = "ow7dxy58glfor9tnc2ansdfoletkfjcv";
+
+        public static byte[] EncryptString(string toEncrypt)
+        {
+            var key = GetKey(password);
+
+            using (var aes = Aes.Create())
+            using (var encryptor = aes.CreateEncryptor(key, key))
+            {
+                var plainText = Encoding.UTF8.GetBytes(toEncrypt);
+                return encryptor.TransformFinalBlock(plainText, 0, plainText.Length);
+            }
+        }
+
+        public static string DecryptToString(byte[] encryptedData)
+        {
+            var key = GetKey(password);
+
+            using (var aes = Aes.Create())
+            using (var encryptor = aes.CreateDecryptor(key, key))
+            {
+                var decryptedBytes = encryptor
+                    .TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+
+        private static byte[] GetKey(string password)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(password);
+            using (var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(keyBytes);
+            }
+        }
+    }
+}
---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		bool isCrypted;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+			isCrypted = false;
+        }
+
+		private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+			btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+			btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+			btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+			cmbFontFamily.SelectedItem = temp;
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+			cmbFontSize.Text = temp.ToString();
+		}
+
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+			if (dlg.ShowDialog() == true)
+			{
+				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+				range.Load(fileStream, DataFormats.Rtf);
+			}
+		}
+
+		private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			SaveFileDialog dlg = new SaveFileDialog();
+			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+			if (dlg.ShowDialog() == true)
+			{
+				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+				range.Save(fileStream, DataFormats.Rtf);
+			}
+		}
+
+		private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (cmbFontFamily.SelectedItem != null)
+				rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+		}
+
+		private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+		}
+
+		private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+		{
+			if (isCrypted)
+			{
+				rtbEditor.Selection.Text = Crypting.DecryptToString(Convert.FromBase64String(rtbEditor.Selection.Text));
+				btnEncrypt.IsChecked = false;
+			}
+			else
+				MessageBox.Show("Надо с начала зашифровать");
+		}
+
+		private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+		{
+			rtbEditor.Selection.Text = Convert.ToBase64String(Crypting.EncryptString(rtbEditor.Selection.Text));
+			isCrypted = true;
+			btnDecrypt.IsChecked = false;
+		}
+	}
+}
